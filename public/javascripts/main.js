b0f9bd03c02e4e66bc65143209cd6e3642bba5ee
Spencer Spiegel <spencer@Spencers-MacBook-Pro.local>
2015-03-03 11:40:10 -0800

    updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,24 @@
 $(document).on('ready' , function(){
 
-	function initialize() {
-	  var mapOptions = {
-	    zoom: 17,
-	    center: new google.maps.LatLng($('#latitude'), $('#longitude'))
-	  };
+	// function initialize() {
+	//   var mapOptions = {
+	//     zoom: 17,
+	//     center: new google.maps.LatLng($('#latitude'), $('#longitude'))
+	//   };
 
-	  var map = new google.maps.Map(document.getElementById('map-canvas'),
-	      mapOptions);
+	//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+	//       mapOptions);
 
-	  var marker = new google.maps.Marker({
-	      position: new google.maps.LatLng($('#latitude'), $('#longitude')),
-	      map: map,
-	      title: '301 N. Canon'
-	  });
+	//   var marker = new google.maps.Marker({
+	//       position: new google.maps.LatLng($('#latitude'), $('#longitude')),
+	//       map: map,
+	//       title: '301 N. Canon'
+	//   });
 
-	};
+	// };
 
 
-	google.maps.event.addDomListener(window, 'load', initialize)
+	// google.maps.event.addDomListener(window, 'load', initialize)
 
 	$('.thumb').on('click' , function(){
 		$('.carousel').carousel($(this).data('carousel-number'))
